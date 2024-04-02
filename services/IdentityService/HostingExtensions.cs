fe52bbdb9511219a4f8c29910ba1ef932092383c
Oleksii Bubenko <ipz202_bov@student.ztu.edu.ua>
2024-04-02 21:21:06 +0300

    Add test version of login and register logic

--- services/IdentityService/HostingExtensions.cs
+++ services/IdentityService/HostingExtensions.cs
@@ -36,6 +36,11 @@ internal static class HostingExtensions
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
 
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.SameSite = SameSiteMode.Lax;
+        });
+
         builder.Services.AddAuthentication();
         //.AddGoogle(options =>
         //{
